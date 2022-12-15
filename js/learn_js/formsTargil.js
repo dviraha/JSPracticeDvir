7a7534822369f25b708e8f46dd4ad12b0f7c8d7c
dviraha <dvir.aha@gmail.com>
2022-12-15 18:27:20 -0500

    .

--- js/learn_js/formsTargil.js
+++ js/learn_js/formsTargil.js
@@ -1,39 +1,15 @@
-// const { format } = require("path")
-
 const aName = document.getElementById('name')
 const aPassword = document.getElementById('password')
 const aSubmit = document.getElementById('submit')
 const aList = document.getElementById('list')
-
-// form.addEventListener('submit' => {
-//     if (name.value === 'dvir' && password === 1234) {
-//         return true
-//     } else {
-//         return false
-//     }
-// })
-
-    // let activated = true;
-    // const button = document.getElementById('myButton');
-    // const input = document.getElementById('myInput'); 
-    // const list = document.getElementById('list');
     aSubmit.addEventListener('click', (event) => { 
-        //console.log('clickssss'  + " " + input.value); 
         const myListItem = document.createElement('li'); 
-        // console.log(input.value);
         myListItem.innerHTML = aName.value + " " + aPassword.value;
         if ((aName.value == 'dvir') && (aPassword.value == '1234')) {
             myListItem.style.color = "green";
         } else {
            myListItem.style.color = "red";
         }
-        // const style = document.createElement('style');
-        // style.innerHTML = `
-        // .note {
-        //     background-color: red;
-        // }
-        // `;
-        // aList.head.appendChild(style);
         aList.appendChild(myListItem);
         event.preventDefault();
     })
